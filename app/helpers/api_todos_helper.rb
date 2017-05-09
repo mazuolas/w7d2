fe27be6b318ba0fcd323b210717d4a25814813f9
Mark Azuolas <markazuolas@gmail.com>
2017-05-09 11:08:10 -0700

    Initial commit

---
+++ app/helpers/api_todos_helper.rb
@@ -0,0 +1,2 @@
+module ApiTodosHelper
+end
---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+t1 = Todo.create(title: "todo1", body: "yeah")
+t2 = Todo.create(title: "todo2", body: "yeah")
+t3 = Todo.create(title: "todo3", body: "yeah")
